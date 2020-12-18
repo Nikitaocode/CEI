6592cecde1cf876ab6a715826bdddd4f07f46320
Nikita Chanana <nikitachanana0106@gmail.com>
2020-12-18 13:49:34 +0530

    buildd

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const mainRoutes = require("./Routes/MainRouter")
 require('dotenv').config()
+app.use(express.static('client/build'))
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+})
 const port = process.env.PORT || 5000
 app.use(express.json());
 app.use("/", mainRoutes)
